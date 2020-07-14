21a8d23823dddab001979cbe829a956cab44e3cd
Crowdbotics <team@crowdbotics.com>
2020-07-14 02:25:13 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -3,11 +3,19 @@ import {
   REGISTER_USERNAME,
   REGISTER_PASSWORD,
   PARENT_ONLY_USERNAME,
+  PARENT_ONLY_PASSWORD,
+  PARENT_ONLY_USERNAME,
   PARENT_ONLY_PASSWORD
 } from "react-native-dotenv"
 const register = axios.create({
   baseURL: "https://app3.jackrabbitclass.com/regv2.asp?id=540191",
   auth: { username: REGISTER_USERNAME, password: REGISTER_PASSWORD },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+const parentOnly = axios.create({
+  baseURL:
+    "https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgID=540191",
+  auth: { username: PARENT_ONLY_USERNAME, password: PARENT_ONLY_PASSWORD },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 const parentOnly = axios.create({
